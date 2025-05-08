940686beca791ed8564c3521d9394e1118814b8c
Zadvenul <twetwetwe12e@gmail.com>
2025-05-08 16:43:03 +0500

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,8 +1,1 @@
-queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
-
-local TeleportService = game:GetService("TeleportService")
-
-TeleportService:Teleport(131821755744402, game.Players.LocalPlayer, TeleportService:GetLocalPlayerTeleportData())
-
-queueteleport()
-
+print("Hello motherfucker")
